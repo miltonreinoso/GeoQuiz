0b4313009cc51deac6f6f5d73eb4c5c655dad1b6
milton <reinoso.vm@gmail.com>
2018-07-04 19:04:35 -0300

    Add Question model class, add questions to strings.xml

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,19 +2,44 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
-public class QuizActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     private Button mTrueButton;
     private Button mFalseButton;
+    ArrayList<Question> mQuestionAL= new ArrayList <>();
+    int mCurrentIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        /* True and False buttons with an OnClickListener  */
+
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
         mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+
+
     }
 }
